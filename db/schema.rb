e886969c58211b2818917191074f96b2b580c43c
xueshengyt <thefootmark@163.com>
2016-12-31 17:03:52 +0800

    install devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161231042416) do
+ActiveRecord::Schema.define(version: 20161231082123) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
